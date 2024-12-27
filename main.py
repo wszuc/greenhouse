e0be108de959f70392e553f79ba321cf7b6805d4
wszuc <wiktor.szuca@gmail.com>
2024-12-27 19:38:57 +0000

    API extended

--- main.py
+++ main.py
@@ -1,7 +1,71 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Query
+from pydantic import BaseModel
+from typing import Annotated
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+mocked_readings = {"temperature": 22.3, "humidity": 66, "lighting": 44}
+
+class ConditionsSetBase(SQLModel):
+    temperature: float | None = None
+    humidity: float | None = None
+    lighting: float | None = None
+    comment: str | None = None
+
+class ConditionsSet(ConditionsSetBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    secret_key: str
+
+class ConditionsSetPublic(ConditionsSetBase):
+    id: int
+
+class ConditionsSetCreate(ConditionsSetBase):
+    secret_key: str  # Usunięto `Field(default=None)` aby pole było wymagane
+
+class ConditionsSetUpdate(ConditionsSetBase):
+    secret_key: str | None = None  # Opcjonalne dla aktualizacji
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+@app.post("/set_conditions/", response_model=ConditionsSetPublic)
+async def set_conditions(new_conditions: ConditionsSetCreate, session: SessionDep):
+    db_conditions = ConditionsSet.model_validate(new_conditions)
+    session.add(db_conditions)
+    session.commit()
+    session.refresh(db_conditions)
+    return db_conditions
+
+@app.patch("/update_conditions/{conditions_set_id}", response_model=ConditionsSetPublic)
+async def update_conditions(conditions_set_id: int, updated_conditions: ConditionsSet, session: SessionDep):
+    db_conditions = session.get(ConditionsSet, conditions_set_id)
+    if not db_conditions:
+        raise HTTPException(status_code=404, detail="No object found!")
+    received_data = updated_conditions.model_dump(exclude_unset=True)
+    db_conditions.sqlmodel_update(received_data)
+    session.add(db_conditions)
+    session.commit()
+    session.refresh(db_conditions)
+    return db_conditions
+
+@app.get("/get_conditions/", response_model=list[ConditionsSetPublic])
+async def get_conditions(session: SessionDep) -> list[ConditionsSet]:
+    conditions_sets = session.exec(select(ConditionsSet)).all()
+    return conditions_sets
