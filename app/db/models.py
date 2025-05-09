9dfdbf79214c20be28d333909d0dbcf20253e1ca
wszuc <wiktor.szuca@gmail.com>
2025-05-09 18:58:31 +0200

    models added

---
+++ app/db/models.py
@@ -0,0 +1,17 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from pydantic import field_validator
+
+# absctract base-class
+class ConditionsSetBase(SQLModel):
+    temp_1: float = None
+    temp_2: Optional[float] = None
+    temp_3: Optional[float] = None
+    humidity: float = Field(default=0, ge=0)  
+    lighting: float = Field(default=0, ge=0) 
+    comment: Optional[str] = None
+
+# real table in db
+class ConditionsSet(ConditionsSetBase, table=True):
+    id: int = Field(primary_key=True)
+    secret_key: str
