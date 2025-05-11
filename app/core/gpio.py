e4038f32cc8bf379929fb06fd92ac813659b65f8
wszuc <wiktor.szuca@gmail.com>
2025-05-11 17:40:07 +0200

    changes in script

--- app/core/gpio.py
+++ app/core/gpio.py
@@ -5,7 +5,7 @@ from typing import Optional,  Dict
 import w1thermsensor
 
 led = LED(17)
-dht = adafruit_dht.DHT11(board.D27, use_pulseio=False)
+dht = adafruit_dht.DHT11(board.D27)
 sensor = w1thermsensor.W1ThermSensor()
 
 
