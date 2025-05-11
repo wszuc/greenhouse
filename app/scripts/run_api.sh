fb4eb7d39b8c4644e51307dcf5d0faa8f6f55975
wszuc <wiktor.szuca@gmail.com>
2025-05-11 16:50:23 +0200

    changes in bash scripts

--- app/scripts/run_api.sh
+++ app/scripts/run_api.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/wiktor/Desktop/greenhouse
 source bin/activate
-PYTHONPATH=. uvicorn app.main:app --host 0.0.0.0 --port 8000
+PYTHONPATH=. fastapi dev app/main.py --host 0.0.0.0 --port 8000
