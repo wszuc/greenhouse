93def6e21f18847613ff569b789026c86b142261
wszuc <wiktor.szuca@gmail.com>
2025-05-27 21:29:38 +0200

    changes changes

--- app/api/endpoints/actuators.py
+++ app/api/endpoints/actuators.py
@@ -1,16 +1,17 @@
 # Set of endpoints for steering actuators
 
 from fastapi import APIRouter
-from app.core.gpio import heater_off, heater_on
+from app.core.gpio import GPIO
 
 router = APIRouter()
+gpio = GPIO()
 
 @router.post("/heater-on")
 def turn_on():
-    heater_on()
-    return {"status": "LED ON"}
+    gpio.heater_on()
+    return {"status": "HEATER ON"}
 
 @router.post("/heater-off")
 def turn_off():
-    heater_off()
-    return {"status": "LED OFF"}
+    gpio.heater_off()
+    return {"status": "HEATER OFF"}
--- app/api/endpoints/led.py
+++ app/api/endpoints/led.py
@@ -1,16 +1,17 @@
 # Set of endpoints mainly for testing purposes - turning LED on and off
 
 from fastapi import APIRouter
-from app.core.gpio import led_on, led_off
+from app.core.gpio import GPIO
 
 router = APIRouter()
+gpio = GPIO()
 
 @router.post("/on")
 def turn_on():
-    led_on()
+    gpio.led_on()
     return {"status": "LED ON"}
 
 @router.post("/off")
 def turn_off():
-    led_off()
+    gpio.led_off()
     return {"status": "LED OFF"}
