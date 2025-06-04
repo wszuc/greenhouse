edde800b78d82873c84a64510804327a6e4a6708
wszuc <wiktor.szuca@gmail.com>
2025-06-04 20:14:27 +0200

    mqqt endpoint

--- app/api/endpoints/sensors.py
+++ app/api/endpoints/sensors.py
@@ -1,5 +1,6 @@
 # Endpoints returning actual conditons in the greenhouse. Return n last records from conditionsset table.
 from datetime import datetime
+import os
 from fastapi import FastAPI, HTTPException, Query, APIRouter
 from sqlmodel import Field, Session, SQLModel, select
 from app.db.models import ConditionsSet, ConditonsSetPublic
@@ -37,3 +38,32 @@ def get_values(limit: int = 1):
     with Session(engine) as session:
         values = session.exec(select(ConditionsSet).order_by(ConditionsSet.id.desc()).limit(limit)).all()
         return values
+    
+@router.get("/read_mqqt/", response_model=list[ConditonsSetPublic])
+def read_live_conditions():
+    temperature = gpio.get_temperature()
+    ht_data = gpio.get_humidity_and_temperature()
+    soil_humidity = gpio.get_soil_humidity()
+    lighting = gpio.get_lighting()
+
+    if temperature is None or ht_data is None:
+        raise HTTPException(status_code=500, detail="Błąd odczytu z czujników")
+
+    readings = {
+        "id": 0,
+        "uid": "raspberry",
+        "temp_1": temperature,
+        "temp_2": ht_data["temperature"],
+        "temp_3": 0.0,
+        "humidity": ht_data["humidity"],
+        "soil_humidity": soil_humidity,
+        "lighting": lighting,
+        "date": datetime.now().astimezone()
+    }
+
+    # send data via MQQT to thingsboard
+
+    result = os.system(f'mosquitto_pub -d -q 1 -h demo.thingsboard.io -p 1883 -t v1/devices/me/telemetry -u "eac44v98ye7olt0al0ge" -m "${readings}"')
+
+    return result
+    
