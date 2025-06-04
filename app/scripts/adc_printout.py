a582dd36205612fe0e12afbcfcfbe2e627f5950f
wszuc <wiktor.szuca@gmail.com>
2025-06-04 19:09:54 +0200

    changes

--- app/scripts/adc_printout.py
+++ app/scripts/adc_printout.py
@@ -1,13 +1,13 @@
 import time
 import board
+import busio
 import digitalio
-import bitbangio
 from adafruit_mcp3xxx.mcp3008 import MCP3008
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
-# Ustawienie SPI na niestandardowych pinach
-spi = bitbangio.SPI(clock=board.D11, MISO=board.D13, MOSI=board.D19)
-cs = digitalio.DigitalInOut(board.D26)  # CS
+# Ustawienie SPI na sprzętowych pinach
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.D25)  # lub board.CE0 jeśli używasz GPIO 8
 
 # Inicjalizacja MCP3008
 mcp = MCP3008(spi, cs)
