dd221462706673769f6911219c22d21464950885
wszuc <wiktor.szuca@gmail.com>
2025-10-07 20:25:22 +0200

    i2c test

--- app/scripts/i2c_test.py
+++ app/scripts/i2c_test.py
@@ -1,10 +1,18 @@
-import smbus2
+import time
+import board
+import busio
+from app.external_libs.DFRobot_AHT20 import DFRobot_AHT20
 
-bus = smbus2.SMBus(1)  
-address = 0x38         
+i2c = busio.I2C(board.SCL, board.SDA)
+aht = DFRobot_AHT20()
 
-try:
-    bus.write_quick(address)  
-    print(f"Urządzenie znalezione pod adresem 0x{address:02X}")
-except OSError:
-    print(f"Brakk urządzenia pod adresem 0x{address:02X}")
+print("Initializing AHT20...")
+if not aht.begin():
+    print("AHT20 not detected!")
+    exit(1)
+
+while True:
+    temp = aht.get_temperature()
+    hum = aht.get_humidity()
+    print(f"Temp: {temp:.2f}°C, Hum: {hum:.2f}%")
+    time.sleep(2)
