b518d53620f2974f1bee9be97691a53c1f24d9a7
wszuc <wiktor.szuca@gmail.com>
2025-10-18 15:13:47 +0200

    fixed typo

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app.include_router(led.router, prefix="/led", tags=["LED"])
 app.include_router(sensors.router, prefix="/sensors", tags=["Sensors"])
 app.include_router(actuators.router, prefix="/actuators", tags=["Actuators"])
 app.include_router(sync.router, prefix="/synchronize-data", tags=["Synced"])
-app.include_router(sync.router, prefix="/climate", tags=["Climate"])
+app.include_router(climate.router, prefix="/climate", tags=["Climate"])
 
 @app.on_event("startup")
 def on_startup():
